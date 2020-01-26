7aa4fec1b281ed1ad3e8a48893aef2a5905620b2
DIMITRIOS MARKANTONATOS <dim13_94@hotmail.com>
2020-01-26 19:01:17 +0200

    working admin login

--- login.php
+++ login.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
-?>
-<!--<html>-->
-<!--<head>-->
-<!--<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>-->
-<!--</head>-->
-<!--</html>-->
-<?php
+
+//<html>
+//<head>
+//<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+//</head>
+//</html>
+
 
 
 $db_server["host"] = "localhost"; //database server
@@ -15,6 +15,12 @@ $db_server["password"] = ""; // DB password
 $db_server["database"] = "web20";// database name
 
 $mysql_con = mysqli_connect($db_server["host"], $db_server["username"], $db_server["password"], $db_server["database"]);
+
+if (mysqli_connect_error())
+{
+    die('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
+}
+echo "Success!";
 
 $mysql_con->query ('SET CHARACTER SET utf8');
 $mysql_con->query ('SET COLLATION_CONNECTION=utf8_general_ci');
@@ -28,7 +34,7 @@ $result = $mysql_con->query($my_query);
 
 
 
-if (isset($_SESSION['session_username']) || isset($_SESSION['session_password']))
+if (!isset($_SESSION['session_username']) || !isset($_SESSION['session_password']))
 {
     if (strcmp($_POST['username'], 'admin') || strcmp($_POST['password'], 'admin'))
 	{
--- loginform.php
+++ loginform.php
@@ -1,16 +1,48 @@
-<html>
+<!--<html>-->
+<!--<head>-->
+<!--<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>-->
+<!--</head>-->
+<!--<body>-->
+<!---->
+<!--<form name="login_form" method="POST" action="login.php">-->
+<!--Username: <input type="text" name="username"><br>-->
+<!--Password: <input type="password" name="password"><br>-->
+<!--<input type="submit" value="Login">-->
+<!--</form>-->
+<!---->
+<!---->
+<!---->
+<!--</body>-->
+<!--</html>-->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<title>log in screen</title>
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<!--    <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+    <link rel="stylesheet" href="loginform.css">
 </head>
-<body>
 
-<form name="login_form" method="POST" action="login.php">
-Username: <input type="text" name="username"><br>
-Password: <input type="password" name="password"><br>
-<input type="submit" value="Login">
-</form>
+<body background="images/patras.jpg">
 
+<h1>Welcome to our site!</h1>
 
+<div class="log">
+    <form name="login_form" method="POST" action="login.php" >
+        Username:
+        <input type="text" name="username" placeholder="username" id="username">
+        <br><br>
+        Password:
+        <input style="margin-left: 4px" type="password" name="password" placeholder="password" id="password" a>
+        <br><br>
+        <input type="submit" value="Login">
+    </form>
+
+</div>
 
 </body>
+
 </html>
